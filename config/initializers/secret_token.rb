8eb3d0b5cce6474f3b22b46685b90feacc05ad44
Raul Pino <pinox101@gmail.com>
2012-03-27 23:40:48 -0430

    alpha prime

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alphatest::Application.config.secret_token = '176848fce05f47281443b1851d43dabe5b632d4c78b948faf85cd0a4f8a3627b07803d9187d3d8cb90c57e0be69d145c08ff968fbd172bbb4a1f8ac161ff6d59'
