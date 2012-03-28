00eacc9d35839fe757a66ea60a03ad4bb52da2e2
Raul Pino <pinox101@gmail.com>
2012-03-28 13:33:38 -0430

    testing bd2

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,12 @@
 class DashboardController < ApplicationController
   
   def index
-  	# a = Star.new
-  	# a.star = 12
-  	# a.save
+  	a = Star.new
+  	a.star = 12
+  	a.save
 
   	@b = Star.find_by_id(1)
-  	
+
   end
   
 end
